cc503793a671460e7deb17967fa84c9c3aa4f592
tananaa <tanyaknz07@gmail.com>
2026-06-09 09:45:20 +0300

    rename to gtests.yml

---
+++ _build_tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tanya/tananaa/workspace/rk2_new
+# Build directory: /home/tanya/tananaa/workspace/rk2_new/_build_tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests "/home/tanya/tananaa/workspace/rk2_new/_build_tests/tests")
+set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/home/tanya/tananaa/workspace/rk2_new/CMakeLists.txt;12;add_test;/home/tanya/tananaa/workspace/rk2_new/CMakeLists.txt;0;")
